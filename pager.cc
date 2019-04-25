1343b66430e78a0044591f5eb9a866ce3d638008
jian13579 <jhl3@williams.edu>
2019-04-25 16:11:39 -0400

    adding checks

--- pager.cc
+++ pager.cc
@@ -166,7 +166,7 @@ int vm_fault(void *addr, bool write_flag){
   //Zero when zeroPage is being used using memset
   if(toUpdate->zero){
     //cout <<"Page Number: " <<  ppage_num << endl;
-    cout << "Zeroing Page: " << ((unsigned long) pm_physmem + (ppage_num * VM_PAGESIZE)), 0 , VM_PAGESIZE) << endl;
+    cout << "Zeroing Page: " << ((unsigned long) pm_physmem + (ppage_num * VM_PAGESIZE))) << endl;
     memset((char *) ((unsigned long) pm_physmem + (ppage_num * VM_PAGESIZE)), 0 , VM_PAGESIZE);
     toUpdate->zero = 0;
   }
@@ -286,7 +286,7 @@ int vm_syslog(void *message, unsigned int len){
       ppage_num = toaccess->ppage;
     }
     cout << "PPage_num: " << ppage_num << endl; 
-    cout << (char *) (ppage_num * VM_PAGESIZE) << endl;
+    cout << (ppage_num * VM_PAGESIZE) << endl;
 
     unsigned long start = (unsigned long) pm_physmem + ((unsigned long) ppage_num * (unsigned long) VM_PAGESIZE);
     unsigned long end = start + (unsigned long) VM_PAGESIZE;
